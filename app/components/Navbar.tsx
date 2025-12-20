4bba578c8240862908931b3c5215ec6f3d2846da
Nikhil Chauhan <nikhil.chauhan9869@gmail.com>
2025-12-20 20:27:10 +0530

    home page implemented partially

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,18 @@
 import Link from "next/link";
+import Logo from "./Logo";
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
-    <nav style={{ padding: "1rem", borderBottom: "1px solid #eee" }}>
-      <p className="logo">Quickiz</p>
-      <Link href="/">Home</Link>{" | "}
-      <Link href="/visualizer">Visualizer</Link>{" | "}
-      <Link href="/algorithms">Algorithms</Link>{" | "}
-      <Link href="/playground">Playground</Link>
+    <nav className={styles.navbar}>
+      <Logo />
+
+      <div className={styles.links}>
+        <Link href="/">Home</Link>
+        <Link href="/visualizer">Visualizer</Link>
+        <Link href="/algorithms">Algorithms</Link>
+        <Link href="/playground">Playground</Link>
+      </div>
     </nav>
   );
 }
