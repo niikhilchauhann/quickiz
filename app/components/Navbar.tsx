d5ae419e95653bd13cfa73be41cc85ab8d3b428d
Nikhil Chauhan <nikhil.chauhan9869@gmail.com>
2025-12-20 18:12:31 +0530

    setup app router and base routes

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav style={{ padding: "1rem", borderBottom: "1px solid #eee" }}>
+      <Link href="/">Home</Link>{" | "}
+      <Link href="/visualizer">Visualizer</Link>{" | "}
+      <Link href="/algorithms">Algorithms</Link>{" | "}
+      <Link href="/playground">Playground</Link>
+    </nav>
+  );
+}
